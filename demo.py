3281b1c0606f14c1c5e9346777ff8379721b2e1d
ruizhi <ruizhiwang11@gmail.com>
2023-11-13 12:47:19 +0800

    lfs

--- demo.py
+++ demo.py
@@ -1,67 +1,57 @@
 import pandas as pd
-from itertools import combinations
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
 from collections import defaultdict
 
-# Define the Apriori function for processing a single chunk
-def apriori_on_chunk(chunk, min_support):
-    # Here we assume chunk is a list of lists (transactions)
-    # Each inner list is a transaction and contains items
+# Function to encode a chunk of transactions
+def encode_transactions(transactions, te):
+    te_ary = te.transform(transactions)
+    return pd.DataFrame(te_ary, columns=te.columns_)
 
-    # Step 1: Count how many times each item appears
-    item_count = defaultdict(int)
-    for transaction in chunk:
-        for item in transaction:
-            item_count[item] += 1
+# Initialize TransactionEncoder
+te = TransactionEncoder()
 
-    # Step 2: Keep only items that meet the min_support
-    num_transactions = len(chunk)
-    frequent_items = {item for item, count in item_count.items() if count / num_transactions >= min_support}
+# Parameters
+chunk_size = 50000  # Adjust chunk size to your system's memory limit
+min_support = 0.01  # Adjust the minimum support value as needed
+total_transactions = 0  # Keep track of total number of transactions
+all_items_set = set()  # This will hold all unique items
+selected_columns = ['Primary Type', 'Arrest', 'Domestic']
 
-    # Step 3: Create combinations of these items to form itemsets
-    # Note: This is a very basic and not optimized way of doing it
-    # In practice, you would use more efficient data structures and algorithms
-    itemsets = list(frequent_items)
-    frequent_itemsets = []
-    for k in range(2, len(itemsets) + 1):
-        for combo in combinations(itemsets, k):
-            itemset = frozenset(combo)
-            itemset_count = sum(1 for transaction in chunk if itemset.issubset(transaction))
-            if itemset_count / num_transactions >= min_support:
-                frequent_itemsets.append((itemset, itemset_count))
+# First pass: create the set of all unique items
+for chunk in pd.read_csv("Crimes_-_2001_to_Present.csv", chunksize=chunk_size):
+    chunk['Arrest'] = chunk['Arrest'].apply(lambda x: 'Arrest' if x else 'No Arrest')
+    chunk['Domestic'] = chunk['Domestic'].apply(lambda x: 'Domestic' if x else 'No Domestic')
+    chunk = chunk.astype(str)
+    transactions = chunk[selected_columns].values.tolist()
+    all_items_set.update([item for sublist in transactions for item in sublist])
 
-    return frequent_itemsets
+# Fit the TransactionEncoder to all items
+te.fit([list(all_items_set)])
 
-# Define the function to read the CSV in chunks
-def get_chunks(filename, chunk_size):
-    return pd.read_csv(filename, chunksize=chunk_size)
+# Initialize dictionary to keep track of itemset support across all chunks
+itemset_support_dict = defaultdict(int)
 
-# Define the main function to run the disk-based Apriori
-def disk_based_apriori(filename, min_support):
-    global_itemsets_counts = defaultdict(int)
+# Second pass: read the CSV file in chunks and process each chunk
+for chunk in pd.read_csv("Crimes_-_2001_to_Present.csv", chunksize=chunk_size):
+    chunk['Arrest'] = chunk['Arrest'].apply(lambda x: 'Arrest' if x else 'No Arrest')
+    chunk['Domestic'] = chunk['Domestic'].apply(lambda x: 'Domestic' if x else 'No Domestic')
+    chunk = chunk.astype(str)
+    transactions = chunk[selected_columns].values.tolist()
+    df_encoded = encode_transactions(transactions, te)
+    chunk_frequent_itemsets = apriori(df_encoded, min_support=min_support, use_colnames=True)
+    for index, row in chunk_frequent_itemsets.iterrows():
+        itemset = frozenset(row['itemsets'])
+        support = row['support']
+        itemset_support_dict[itemset] += support * len(chunk)
+    total_transactions += len(chunk)
 
-    # Process each chunk
-    for chunk in get_chunks(filename, chunk_size=80):
-        # Pre-process the chunk to fit the expected format for the apriori_on_chunk function
-        # Assuming each row is a transaction and we select a subset of columns to consider
-        transactions = chunk[['Primary Type', 'Location Description', 'Arrest', 'Domestic']].values.tolist()
-        # Find frequent itemsets in this chunk
+# Convert support counts to relative support values
+for itemset in itemset_support_dict.keys():
+    itemset_support_dict[itemset] /= total_transactions
 
-        local_frequent_itemsets = apriori_on_chunk(transactions, min_support)
-        print(local_frequent_itemsets)
-        # Merge with global itemsets counts
-        for itemset, count in local_frequent_itemsets:
-            global_itemsets_counts[frozenset(itemset)] += count
-        print(global_itemsets_counts)
+# Filter itemsets by global support to get the final frequent itemsets
+global_frequent_itemsets = {itemset for itemset, support in itemset_support_dict.items() if support >= min_support}
 
-    # Filter global itemsets by min_support (considering all chunks)
-    total_transactions = sum(1 for chunk in get_chunks(filename, chunk_size=80) for _ in chunk.index)
-    global_frequent_itemsets = {itemset: count for itemset, count in global_itemsets_counts.items()
-                                if count / total_transactions >= min_support}
-
-    return global_frequent_itemsets
-
-# Run the disk-based Apriori
-filename = 'Crimes_-_2001_to_Present.csv'
-min_support = 0.01  # e.g., 1%
-frequent_itemsets = disk_based_apriori(filename, min_support)
-print(frequent_itemsets)
+# Display the global frequent itemsets
+print(global_frequent_itemsets)
